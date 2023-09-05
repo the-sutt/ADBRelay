ace86d68dc2579bc01107cc0c0c1e6237d2ed9a3
the-sutt <github@roman55.de>
2023-09-05 22:54:35 +0200

    correct return method for errors / included debug logging

--- index.js
+++ index.js
@@ -10,31 +10,38 @@ const port = 3000
 
 app.post('/connect/:ip', (req, res) => {
   try {
+    console.log('connecting to '+req.params.ip)
     const ret = cp.execSync(`adb connect ${req.params.ip}`)
     res.send(ret)
   }
   catch (ex) {
-    res.errored(ex)
+    console.error('error', ex)
+    res.status(500).send(ex.message)
   }
 })
 
 app.post('/disconnect/:ip', (req, res) => {
   try {
+    console.log('disconnecting from '+req.params.ip)
     const ret = cp.execSync(`adb disconnect ${req.params.ip}`)
     res.send(ret)
   }
   catch (ex) {
-    res.errored(ex)
+    console.error('error', ex)
+    res.status(500).send(ex.message)
   }
 })
 
 app.post('/command', jsonparser, (req, res) => {
   try {
-    const ret = cp.execSync(`adb -s ${req.body.device} ${req.body.command}`)
+    const cmd = `adb -s ${req.body.device} ${req.body.command}`
+    console.log('executing command',cmd)
+    const ret = cp.execSync(cmd)
     res.send(ret)
   }
   catch (ex) {
-    res.errored(ex)
+    console.error('error', ex)
+    res.status(500).send(ex.message)
   }
 })
 
